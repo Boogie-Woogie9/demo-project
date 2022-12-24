eff73309423c3d1f5be59a84b71136748c1c9c54
Ivan <evseenkov.2003@mail.ru>
2022-12-24 22:38:49 +0300

    User_model

---
+++ db/migrate/20221224192918_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password, null: false
+      t.date :birth_date, null: false
+      t.integer :test_passed
+      t.float :avrg_rate
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
